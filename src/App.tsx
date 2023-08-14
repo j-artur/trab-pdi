98dae84bc07870c69fca90f6248fa46d6bd81f89
guilermmm <guilherme.r.a.melo@gmail.com>
2023-08-13 21:25:06 -0300

    zoom
    Co-authored-by: João Artur <j-artur@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,34 @@ import {
   createResource,
   createSignal,
   type Component,
-} from "solid-js";
-import { createStore } from "solid-js/store";
-import { clx } from "./utils";
+} from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { clx } from './utils'
 import {
   Img,
   Operation,
   OperationConfig,
   Transformation,
   TransformationConfig,
+  Zoom,
   createURL,
   getPixels,
   operate,
   operations,
   transform,
   transformations,
-} from "./utils/img";
+  zoom,
+  zooms,
+  ZoomConfig,
+} from './utils/img'
 
 const App: Component = () => {
-  const [images, setImages] = createSignal<Img[]>([]);
+  const [images, setImages] = createSignal<Img[]>([])
   const [opCfg, setOpCfg] = createSignal<OperationConfig>({
-    onOutOfRange: "clamp",
-  });
+    onOutOfRange: 'clamp',
+  })
   const [transformCfg, setTransformCfg] = createStore<TransformationConfig>({
-    onOutOfRange: "clamp",
+    onOutOfRange: 'clamp',
     translate: {
       x: 0,
       y: 0,
@@ -55,33 +59,35 @@ const App: Component = () => {
       x: 0,
       y: 0,
     },
-  });
+  })
   const [selectedTransformation, setSelectedTransformation] =
-    createSignal<Transformation>("translate");
-  const [outputs, setOutputs] = createSignal<Img[]>([]);
+    createSignal<Transformation>('translate')
+  const [zoomCfg, setZoomCfg] = createSignal<ZoomConfig>({
+    amount: 1,
+  })
 
-  const [primaryImage, setPrimaryImage] = createSignal<number>();
-  const [secondaryImage, setSecondaryImage] = createSignal<number>();
+  const [outputs, setOutputs] = createSignal<Img[]>([])
+
+  const [primaryImage, setPrimaryImage] = createSignal<number>()
+  const [secondaryImage, setSecondaryImage] = createSignal<number>()
 
   createEffect(() => {
-    const primaryImg = images()[primaryImage()!];
+    const primaryImg = images()[primaryImage()!]
 
     if (primaryImg) {
-      setTransformCfg("translate", "x", primaryImg.width / 2);
-      setTransformCfg("translate", "y", primaryImg.height / 2);
-      setTransformCfg("rotate", "origin", "x", primaryImg.width / 2);
-      setTransformCfg("rotate", "origin", "y", primaryImg.height / 2);
+      setTransformCfg('translate', 'x', primaryImg.width / 2)
+      setTransformCfg('translate', 'y', primaryImg.height / 2)
+      setTransformCfg('rotate', 'origin', 'x', primaryImg.width / 2)
+      setTransformCfg('rotate', 'origin', 'y', primaryImg.height / 2)
     }
-  });
+  })
 
   async function loadImages(files: File[]) {
-    const canvas = document.createElement("canvas");
+    const canvas = document.createElement('canvas')
 
-    const imgs = await Promise.all(
-      files.map((file) => getPixels(file, canvas))
-    );
+    const imgs = await Promise.all(files.map(file => getPixels(file, canvas)))
 
-    setImages(imgs);
+    setImages(imgs)
   }
 
   return (
@@ -89,34 +95,34 @@ const App: Component = () => {
       <input
         type="file"
         multiple
-        onInput={(e) => loadImages([...(e.currentTarget.files ?? [])])}
+        onInput={e => loadImages([...(e.currentTarget.files ?? [])])}
         accept="image/*,.pgm"
       />
 
       <Show when={images()}>
-        {(imgs) => (
+        {imgs => (
           <ul class="flex p-2 gap-2 flex-wrap max-w-full">
             <For each={[...imgs()]}>
-              {(img) => (
+              {img => (
                 <li
-                  class={clx("border-2 p-1", {
-                    "border-t-blue-500 border-l-blue-500":
+                  class={clx('border-2 p-1', {
+                    'border-t-blue-500 border-l-blue-500':
                       imgs().indexOf(img) === primaryImage(),
-                    "border-b-red-500 border-r-red-500":
+                    'border-b-red-500 border-r-red-500':
                       imgs().indexOf(img) === secondaryImage(),
                   })}
-                  onClick={(e) => {
+                  onClick={e => {
                     if (e.shiftKey) {
                       if (secondaryImage() === imgs().indexOf(img)) {
-                        setSecondaryImage(undefined);
+                        setSecondaryImage(undefined)
                       } else {
-                        setSecondaryImage(imgs().indexOf(img));
+                        setSecondaryImage(imgs().indexOf(img))
                       }
                     } else {
                       if (primaryImage() === imgs().indexOf(img)) {
-                        setPrimaryImage(undefined);
+                        setPrimaryImage(undefined)
                       } else {
-                        setPrimaryImage(imgs().indexOf(img));
+                        setPrimaryImage(imgs().indexOf(img))
                       }
                     }
                   }}
@@ -133,12 +139,12 @@ const App: Component = () => {
 
       <form
         class="p-2 flex gap-2"
-        onInput={(e) => {
+        onInput={e => {
           setOpCfg({
             ...opCfg(),
             onOutOfRange: (e.target as HTMLInputElement)
-              .value as OperationConfig["onOutOfRange"],
-          });
+              .value as OperationConfig['onOutOfRange'],
+          })
         }}
       >
         <label>
@@ -146,7 +152,7 @@ const App: Component = () => {
             type="radio"
             name="on_out_of_range"
             value="clamp"
-            checked={opCfg().onOutOfRange === "clamp"}
+            checked={opCfg().onOutOfRange === 'clamp'}
           />
           Clamp
         </label>
@@ -155,7 +161,7 @@ const App: Component = () => {
             type="radio"
             name="on_out_of_range"
             value="wrap"
-            checked={opCfg().onOutOfRange === "wrap"}
+            checked={opCfg().onOutOfRange === 'wrap'}
           />
           Wrap
         </label>
@@ -164,19 +170,19 @@ const App: Component = () => {
             type="radio"
             name="on_out_of_range"
             value="normalize"
-            checked={opCfg().onOutOfRange === "normalize"}
+            checked={opCfg().onOutOfRange === 'normalize'}
           />
           Normalize
         </label>
       </form>
       <div
         class={clx({
-          "opacity-50":
+          'opacity-50':
             primaryImage() === undefined || secondaryImage() === undefined,
         })}
       >
         <For each={Object.keys(operations) as Operation[]}>
-          {(op) => (
+          {op => (
             <button
               class="border p-2"
               onClick={async () => {
@@ -184,10 +190,10 @@ const App: Component = () => {
                   op,
                   images()[primaryImage()!],
                   images()[secondaryImage()!],
-                  opCfg()
-                );
+                  opCfg(),
+                )
 
-                setOutputs([...outputs(), img]);
+                setOutputs([...outputs(), img])
               }}
               disabled={
                 primaryImage() === undefined || secondaryImage() === undefined
@@ -206,13 +212,13 @@ const App: Component = () => {
               type="radio"
               name="on_out_of_range"
               value="clamp"
-              onInput={(e) => {
+              onInput={e => {
                 setTransformCfg(
-                  "onOutOfRange",
-                  e.target.value as TransformationConfig["onOutOfRange"]
-                );
+                  'onOutOfRange',
+                  e.target.value as TransformationConfig['onOutOfRange'],
+                )
               }}
-              checked={transformCfg.onOutOfRange === "clamp"}
+              checked={transformCfg.onOutOfRange === 'clamp'}
             />
             Clamp
           </label>
@@ -221,23 +227,23 @@ const App: Component = () => {
               type="radio"
               name="on_out_of_range"
               value="wrap"
-              onInput={(e) => {
+              onInput={e => {
                 setTransformCfg(
-                  "onOutOfRange",
-                  e.target.value as TransformationConfig["onOutOfRange"]
-                );
+                  'onOutOfRange',
+                  e.target.value as TransformationConfig['onOutOfRange'],
+                )
               }}
-              checked={transformCfg.onOutOfRange === "wrap"}
+              checked={transformCfg.onOutOfRange === 'wrap'}
             />
             Wrap
           </label>
         </form>
         <div>
           <For each={Object.keys(transformations) as Transformation[]}>
-            {(tr) => (
+            {tr => (
               <button
-                class={clx("border p-2", {
-                  "bg-blue-500": selectedTransformation() === tr,
+                class={clx('border p-2', {
+                  'bg-blue-500': selectedTransformation() === tr,
                 })}
                 onClick={() => setSelectedTransformation(tr)}
               >
@@ -249,18 +255,18 @@ const App: Component = () => {
 
         <form>
           <Switch>
-            <Match when={selectedTransformation() === "translate"}>
+            <Match when={selectedTransformation() === 'translate'}>
               <label>
                 X:
                 <input
                   type="number"
                   value={transformCfg.translate.x}
-                  onInput={(e) => {
+                  onInput={e => {
                     setTransformCfg(
-                      "translate",
-                      "x",
-                      Number(e.currentTarget.value)
-                    );
+                      'translate',
+                      'x',
+                      Number(e.currentTarget.value),
+                    )
                   }}
                   min={-images()[primaryImage()!]?.width}
                   max={images()[primaryImage()!]?.width}
@@ -271,31 +277,31 @@ const App: Component = () => {
                 <input
                   type="number"
                   value={transformCfg.translate.y}
-                  onInput={(e) => {
+                  onInput={e => {
                     setTransformCfg(
-                      "translate",
-                      "y",
-                      Number(e.currentTarget.value)
-                    );
+                      'translate',
+                      'y',
+                      Number(e.currentTarget.value),
+                    )
                   }}
                   min={-images()[primaryImage()!]?.height}
                   max={images()[primaryImage()!]?.height}
                 />
               </label>
             </Match>
-            <Match when={selectedTransformation() === "rotate"}>
+            <Match when={selectedTransformation() === 'rotate'}>
               <label>
                 Origin X:
                 <input
                   type="number"
                   value={transformCfg.rotate.origin.x}
-                  onInput={(e) => {
+                  onInput={e => {
                     setTransformCfg(
-                      "rotate",
-                      "origin",
-                      "x",
-                      Number(e.currentTarget.value)
-                    );
+                      'rotate',
+                      'origin',
+                      'x',
+                      Number(e.currentTarget.value),
+                    )
                   }}
                   min={0}
                   max={images()[primaryImage()!]?.width}
@@ -306,13 +312,13 @@ const App: Component = () => {
                 <input
                   type="number"
                   value={transformCfg.rotate.origin.y}
-                  onInput={(e) => {
+                  onInput={e => {
                     setTransformCfg(
-                      "rotate",
-                      "origin",
-                      "y",
-                      Number(e.currentTarget.value)
-                    );
+                      'rotate',
+                      'origin',
+                      'y',
+                      Number(e.currentTarget.value),
+                    )
                   }}
                   min={0}
                   max={images()[primaryImage()!]?.height}
@@ -323,30 +329,26 @@ const App: Component = () => {
                 <input
                   type="number"
                   value={transformCfg.rotate.angle}
-                  onInput={(e) => {
+                  onInput={e => {
                     setTransformCfg(
-                      "rotate",
-                      "angle",
-                      Number(e.currentTarget.value)
-                    );
+                      'rotate',
+                      'angle',
+                      Number(e.currentTarget.value),
+                    )
                   }}
                   min={0}
                   max={360}
                 />
               </label>
             </Match>
-            <Match when={selectedTransformation() === "scale"}>
+            <Match when={selectedTransformation() === 'scale'}>
               <label>
                 X:
                 <input
                   type="number"
                   value={transformCfg.scale.x}
-                  onInput={(e) => {
-                    setTransformCfg(
-                      "scale",
-                      "x",
-                      Number(e.currentTarget.value)
-                    );
+                  onInput={e => {
+                    setTransformCfg('scale', 'x', Number(e.currentTarget.value))
                   }}
                 />
               </label>
@@ -355,24 +357,20 @@ const App: Component = () => {
                 <input
                   type="number"
                   value={transformCfg.scale.y}
-                  onInput={(e) => {
-                    setTransformCfg(
-                      "scale",
-                      "y",
-                      Number(e.currentTarget.value)
-                    );
+                  onInput={e => {
+                    setTransformCfg('scale', 'y', Number(e.currentTarget.value))
                   }}
                 />
               </label>
             </Match>
-            <Match when={selectedTransformation() === "reflect"}>
+            <Match when={selectedTransformation() === 'reflect'}>
               <label>
                 X:
                 <input
                   type="checkbox"
                   checked={transformCfg.reflect.x}
-                  onInput={(e) => {
-                    setTransformCfg("reflect", "x", e.currentTarget.checked);
+                  onInput={e => {
+                    setTransformCfg('reflect', 'x', e.currentTarget.checked)
                   }}
                 />
               </label>
@@ -381,24 +379,20 @@ const App: Component = () => {
                 <input
                   type="checkbox"
                   checked={transformCfg.reflect.y}
-                  onInput={(e) => {
-                    setTransformCfg("reflect", "y", e.currentTarget.checked);
+                  onInput={e => {
+                    setTransformCfg('reflect', 'y', e.currentTarget.checked)
                   }}
                 />
               </label>
             </Match>
-            <Match when={selectedTransformation() === "shear"}>
+            <Match when={selectedTransformation() === 'shear'}>
               <label>
                 X:
                 <input
                   type="number"
                   value={transformCfg.shear.x}
-                  onInput={(e) => {
-                    setTransformCfg(
-                      "shear",
-                      "x",
-                      Number(e.currentTarget.value)
-                    );
+                  onInput={e => {
+                    setTransformCfg('shear', 'x', Number(e.currentTarget.value))
                   }}
                 />
               </label>
@@ -407,12 +401,8 @@ const App: Component = () => {
                 <input
                   type="number"
                   value={transformCfg.shear.y}
-                  onInput={(e) => {
-                    setTransformCfg(
-                      "shear",
-                      "y",
-                      Number(e.currentTarget.value)
-                    );
+                  onInput={e => {
+                    setTransformCfg('shear', 'y', Number(e.currentTarget.value))
                   }}
                 />
               </label>
@@ -420,17 +410,17 @@ const App: Component = () => {
           </Switch>
           <div>
             <button
-              class={clx("border p-2", {
-                "opacity-50": primaryImage() === undefined,
+              class={clx('border p-2', {
+                'opacity-50': primaryImage() === undefined,
               })}
               onClick={async () => {
                 const img = await transform(
                   selectedTransformation(),
                   images()[primaryImage()!],
-                  transformCfg
-                );
+                  transformCfg,
+                )
 
-                setOutputs([...outputs(), img]);
+                setOutputs([...outputs(), img])
               }}
               disabled={primaryImage() === undefined}
             >
@@ -440,11 +430,43 @@ const App: Component = () => {
         </form>
       </div>
 
+      <div>
+        <For each={Object.keys(zooms) as Zoom[]}>
+          {z => (
+            <button
+              class="border p-2"
+              onClick={async () => {
+                const img = await zoom(z, images()[primaryImage()!], zoomCfg())
+
+                setOutputs([...outputs(), img])
+              }}
+              disabled={primaryImage() === undefined}
+            >
+              {zooms[z]}
+            </button>
+          )}
+        </For>
+      </div>
+      <form>
+        <label>
+          Amount:
+          <input
+            type="number"
+            value={zoomCfg().amount}
+            onInput={e => {
+              setZoomCfg({
+                amount: Number(e.currentTarget.value),
+              })
+            }}
+          />
+        </label>
+      </form>
+
       <Show when={outputs()}>
-        {(outputs) => (
+        {outputs => (
           <ul class="flex gap-2 p-2 flex-wrap max-w-full">
             <For each={[...outputs()]}>
-              {(img) => (
+              {img => (
                 <li title={`${img.width}x${img.height}`}>
                   <Thumbnail img={img} />
                 </li>
@@ -454,11 +476,11 @@ const App: Component = () => {
         )}
       </Show>
     </div>
-  );
-};
+  )
+}
 
 const Thumbnail = ({ img }: { img: Img }) => {
-  const [url] = createResource(img, createURL);
+  const [url] = createResource(img, createURL)
 
   return (
     <div class="h-56 w-56 flex items-center justify-center">
@@ -466,7 +488,7 @@ const Thumbnail = ({ img }: { img: Img }) => {
         <img src={url()} class="object-contain border max-w-full max-h-full" />
       </Suspense>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
